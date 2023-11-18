ab6f0a89e275b60fe8eb7c738b80c337c640acdb
Ng Qing Yu Randy <randyng1998@yahoo.com>
2023-11-18 19:23:01 +0800

    I wrongly put -1 and 0.1 previously.

--- scripts/ml_models.R
+++ scripts/ml_models.R
@@ -23,8 +23,8 @@ train_0302 <- read.csv(here('outputs/baseline_and_network_metrics_0302.csv'))
 ########################################################################
 
 # Replacing NA values for 'transitivity' columns with the value -1.
-train_0302$from_transitivity[is.na(train_0302$from_transitivity)] <- 0.1
-train_0302$to_transitivity[is.na(train_0302$to_transitivity)] <- 0.1
+train_0302$from_transitivity[is.na(train_0302$from_transitivity)] <- -1
+train_0302$to_transitivity[is.na(train_0302$to_transitivity)] <- -1
 
 # scaling the dataset first
 scaled_train_0302 <- scale_dataset(train_0302)
@@ -79,8 +79,8 @@ saveRDS(model3, file = here("outputs/ml_models/scaled_logistic_model3_0302.rds")
 train_0505 <- read.csv(here('outputs/baseline_and_network_metrics_0505.csv'))
 
 # Replacing NA values for 'transitivity' columns with the value -1.
-train_0505$from_transitivity[is.na(train_0505$from_transitivity)] <- 0.1
-train_0505$to_transitivity[is.na(train_0505$to_transitivity)] <- 0.1
+train_0505$from_transitivity[is.na(train_0505$from_transitivity)] <- -1
+train_0505$to_transitivity[is.na(train_0505$to_transitivity)] <- -1
 
 # scaling the dataset
 scaled_train_0505 <- scale_dataset(train_0505)
