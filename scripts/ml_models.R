86700dff78fd95a0dc555d33b07187c124f85708
Ng Qing Yu Randy <randyng1998@yahoo.com>
2023-11-18 14:33:42 +0800

    Change ML model script to include scaling first. Then produced models trained on scaled data.

--- scripts/ml_models.R
+++ scripts/ml_models.R
@@ -3,25 +3,44 @@ library(dplyr)
 setwd(here())
 ########################################################################
 ########################################################################
+
+# function to scale data
+scale_dataset <- function(df, dependent_var = 'connected') {
+  # Identify the column indices of the independent variables
+  independent_vars <- setdiff(names(df), dependent_var)
+  
+  # Scale the independent variables
+  df[independent_vars] <- scale(df[independent_vars])
+  
+  # Ensure the dependent variable is a factor if it's not already
+  df[[dependent_var]] <- as.factor(df[[dependent_var]])
+  
+  return(df)
+}
+
 # LOAD train full data
 train_0302 <- read.csv(here('outputs/baseline_and_network_metrics_0302.csv'))
 ########################################################################
+
+# scaling the dataset first
+scaled_train_0302 <- scale_dataset(train_0302)
+
 # MODEL 1
 # baseline features:
-colnames(train_0302)[3:10]
+colnames(scaled_train_0302)[3:10]
 # [1] "connected"        "num_common_genre"
 # [3] "from_salesrank"   "from_rating"     
 # [5] "from_reviews"     "to_salesrank"    
 # [7] "to_rating"        "to_reviews" 
 
-train_0302_model1 <- train_0302[colnames(train_0302)[3:10]]
+scaled_train_0302_model1 <- scaled_train_0302[colnames(scaled_train_0302)[3:10]]
 
 # Model 1: Using only the baseline features
-model1 <- glm(connected ~ ., data = train_0302_model1, family = "binomial")
+model1 <- glm(connected ~ ., data = scaled_train_0302_model1, family = "binomial")
 
-saveRDS(model1, file = here("outputs/ml_models/logistic_model1_0302.rds"))
+saveRDS(model1, file = here("outputs/ml_models/scaled_logistic_model1_0302.rds"))
 
-colnames(train_0302)[c(3, c(11:23))]
+colnames(scaled_train_0302)[c(3, c(11:23))]
 # [1] "connected"               
 # [2] "from_degree"             
 # [3] "from_closeness"          
@@ -39,39 +58,42 @@ colnames(train_0302)[c(3, c(11:23))]
 
 # MODEL 2
 # only network features:
-train_0302_model2 <- train_0302[colnames(train_0302)[c(3, c(11:23))]]
+scaled_train_0302_model2 <- scaled_train_0302[colnames(scaled_train_0302)[c(3, c(11:23))]]
 
-model2 <- glm(connected ~ ., data = train_0302_model2, family = "binomial")
+model2 <- glm(connected ~ ., data = scaled_train_0302_model2, family = "binomial")
 
-saveRDS(model2, file = here("outputs/ml_models/logistic_model2_0302.rds"))
+saveRDS(model2, file = here("outputs/ml_models/scaled_logistic_model2_0302.rds"))
 
 # Model 3: Using all features
-model3 <- glm(connected ~ ., data = train_0302[3:23], family = "binomial")
+model3 <- glm(connected ~ ., data = scaled_train_0302[3:23], family = "binomial")
 
-saveRDS(model3, file = here("outputs/ml_models/logistic_model3_0302.rds"))
+saveRDS(model3, file = here("outputs/ml_models/scaled_logistic_model3_0302.rds"))
 
 
 ########################################################################
 # LOAD 0505 train full data
 train_0505 <- read.csv(here('outputs/baseline_and_network_metrics_0505.csv'))
+
+# scaling the dataset
+scaled_train_0505 <- scale_dataset(train_0505)
 ########################################################################
 # using 0505 network metrics
 # MODEL 1
 # baseline features:
-colnames(train_0505)[3:10]
+colnames(scaled_train_0505)[3:10]
 # [1] "connected"        "num_common_genre"
 # [3] "from_salesrank"   "from_rating"     
 # [5] "from_reviews"     "to_salesrank"    
 # [7] "to_rating"        "to_reviews" 
 
-train_0505_model1 <- train_0505[colnames(train_0505)[3:10]]
+scaled_train_0505_model1 <- scaled_train_0505[colnames(scaled_train_0505)[3:10]]
 
 # Model 1: Using only the baseline features
-model1_0505 <- glm(connected ~ ., data = train_0505_model1, family = "binomial")
+model1_0505 <- glm(connected ~ ., data = scaled_train_0505_model1, family = "binomial")
 
-saveRDS(model1_0505, file = here("outputs/ml_models/logistic_model1_0505.rds"))
+saveRDS(model1_0505, file = here("outputs/ml_models/scaled_logistic_model1_0505.rds"))
 
-colnames(train_0505)[c(3, c(11:23))]
+colnames(scaled_train_0505)[c(3, c(11:23))]
 # [1] "connected"               
 # [2] "from_degree"             
 # [3] "from_closeness"          
@@ -89,14 +111,14 @@ colnames(train_0505)[c(3, c(11:23))]
 
 # MODEL 2
 # only network features:
-train_0505_model2 <- train_0505[colnames(train_0505)[c(3, c(11:23))]]
+scaled_train_0505_model2 <- scaled_train_0505[colnames(scaled_train_0505)[c(3, c(11:23))]]
 
-model2_0505 <- glm(connected ~ ., data = train_0505_model2, family = "binomial")
+model2_0505 <- glm(connected ~ ., data = scaled_train_0505_model2, family = "binomial")
 
-saveRDS(model2_0505, file = here("outputs/ml_models/logistic_model2_0505.rds"))
+saveRDS(model2_0505, file = here("outputs/ml_models/scaled_logistic_model2_0505.rds"))
 
 # Model 3: Using all features
-model3_0505 <- glm(connected ~ ., data = train_0302[3:23], family = "binomial")
+model3_0505 <- glm(connected ~ ., data = scaled_train_0302[3:23], family = "binomial")
 
-saveRDS(model3_0505, file = here("outputs/ml_models/logistic_model3_0505.rds"))
+saveRDS(model3_0505, file = here("outputs/ml_models/scaled_logistic_model3_0505.rds"))
 
